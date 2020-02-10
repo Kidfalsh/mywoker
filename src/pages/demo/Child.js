f01905ffb72f89ef99c1894499294da28ae7381e
Kidfalsh <www.theflashdye@163.com>
2020-02-10 17:33:03 +0800

    s

---
+++ src/pages/demo/Child.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default class Child extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  
+  componentWillMount() {
+    console.log('will mount')
+  }
+  componentDidMount() {
+    console.log('did mount')
+  }
+  componentWillReceiveProps(newProps) {
+    console.log('will props' + newProps.name)
+  }
+  shouldComponentUpdate() {
+    console.log('should update')
+    return true
+  }
+  componentWillUpdate() {
+    console.log('will update')
+  }
+  componentDidUpdate() {
+    console.log('did update')
+  }
+  render(){
+    return <div style={{color:'red',fontSize:20}}>
+      <div>这里是子组件</div>
+      <p>{this.props.name}</p>
+    </div>
+  }
+}
---
+++ src/pages/demo/Life.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Child from './Child'
+import './Life.css'
+export default class Life extends React.Component{
+  
+  state = {
+    count: 0
+  }
+  handleAdd = () =>{
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render(){
+    return <div className='content'>
+      <p>react生命周期介绍</p>
+      <button onClick={this.handleAdd}>点击一下</button>
+      <p>{this.state.count}</p>
+      <Child name={this.state.count}/>
+    </div>
+  }
+}
